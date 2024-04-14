07d483808c078cc58d951c173259524f6b25ebb7
mohamad zoubi <m.zoubi2@hotmail.com>
2024-04-13 20:19:02 -0700

    added testimonies data

---
+++ src/data/Reviews.js
@@ -0,0 +1,50 @@
+import review1 from "../assets/testimonies/review-1.jpg"
+import review2 from "../assets/testimonies/review-2.jpg"
+import review3 from "../assets/testimonies/review-3.jpg"
+import review4 from "../assets/testimonies/review-4.jpg"
+import review5 from "../assets/testimonies/review-5.jpg"
+import review6 from "../assets/testimonies/review-6.jpg"
+
+
+const Reviews = [
+    {
+        name:"Matt Parker",
+        review: "I have taken my 3 yr old son for his first men’s haircut and now I’m glad he has such a fantastic look. I recommend any of the barbers at Barbershop for service and pride in their work. I believe my son would never be allowed to leave the chair without a really handsome cut.",
+        rating: 5,
+        img: review1
+    },
+    {
+        name:"John Miller",
+        review: "These guys know what they are doing, give solid advice, and cut like pros. I’ll surely visit Barbershop again.",
+        rating: 4,
+        img: review2
+    },
+    {
+        name:"Anthony Smith",
+        review: "Polite, professional, charming and courteous team. The best haircut I have ever had! Simon was full of crack, was more than happy to suggest the best cut for me.",
+        rating: 5,
+        img: review3
+    },
+    {
+        name:"Jim McMillan",
+        review: "Down to earth friendly professional service with no pretentious nonsense which you would expect from a barber in this area. Barbers are well trained and experienced and always give a first class haircut according to your requirements and are always happy to give advice for a style that suits you.",
+        rating: 5,
+        img: review4
+    },
+    {
+        name:"David Flores",
+        review: "I’ve recently had my haircut with Harry, and it’s by far the best haircut I’ve ever had! It feels good knowing that I’ve found a barber who can make me look and feel my best.",
+        rating: 4,
+        img: review5
+    },
+    {
+        name:"Patrick Perez",
+        review: "This is without doubt the finest and best value barber shop in the city. I started going here in response to a series of really bad haircuts at shops local to where I live.",
+        rating: 5,
+        img: review6
+    }
+
+]
+
+
+export default Reviews;
